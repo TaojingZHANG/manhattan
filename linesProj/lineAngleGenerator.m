08af98fac54d01540a5b0328dd7c3cc594203cc8
axel <axel.berg@arm.com>
2019-05-15 11:29:23 +0200

    Updates to angle regression
    Signed-off-by: axel <axel.berg@arm.com>

--- linesProj/lineAngleGenerator.m
+++ linesProj/lineAngleGenerator.m
@@ -2,47 +2,46 @@ clear
 rng(0)
 
 %% Generate N random images with 1 line and its rotation angle as label
+%% Update: angle now between pi/4 and 3*pi/4
 
 N = 1000;
+sigma2 = 0.01;
 
 imres = [342, 342];
 lineIms = uint8(zeros(imres(1), imres(2), 1, N));
-labels = zeros(N, 1);
-
-a = -1 + 2 * rand(N, 1);
-b = -1 + 2 * rand(N, 1);
-c = -1 + 2 * rand(N, 1);
+labels = zeros(1, N);
 
 close all
-figure(1)
+%figure('Position', [100 100 100 100]);
 for n = 1:N
-  ax = subplot(1,1,1);
-
-  % Create 1 line from random points
-  p1 = -1 + 2 * rand(2, 1);
-  p2 = -1 + 2 * rand(2, 1);
-  % make sure the points are separated enough
-  while sqrt(sum((p1-p2).^2)) < 0.5
-    p2 = -1 + 2 * rand(2, 1);
-  end
-  plot([p1(1), p2(1)], [p1(2), p2(2)], 'w');
-  hold on
-  
-  % Calculate angle of rotation
-  dx = p2(1) - p1(1);
-  dy = p2(2) - p1(2);
-  
-  labels(n) = atan(dy / dx);
+  theta = 0;
+  while abs(theta) < pi / 4
+    ax = subplot(1,1,1);
     
-  hold off
-
+    % Create 1 line from random points
+    p1 = -1 + 2 * rand(2, 1);
+    p2 = -1 + 2 * rand(2, 1);
+    % make sure the points are separated enough
+    while sqrt(sum((p1-p2).^2)) < 0.5
+      p2 = -1 + 2 * rand(2, 1);
+    end
+    plot([p1(1), p2(1)], [p1(2), p2(2)], 'w');
+    
+    % Calculate angle of rotation
+    dx = p2(1) - p1(1);
+    dy = p2(2) - p1(2);
+    
+    theta = atan(dy / dx);
+  end
+  labels(:, n) = mod(theta, pi);
+    
   % Save plot
   axis([-1, 1, -1, 1])
   pbaspect([1 1 1])
   set(gca,'visible','off')
   set(gcf,'color','k');
   I = frame2im(getframe(ax));
-  lineIms(:, :, :, n) = uint8(rgb2gray(I));
+  lineIms(:, :, :, n) = imnoise(uint8(rgb2gray(I)), 'gaussian', 0, sigma2);
   
 end
 
--- linesProj/lineAngleRegression.m
+++ linesProj/lineAngleRegression.m
@@ -5,44 +5,63 @@ rng(0);
 load('lineAnglesData.mat');
 addpath('../tools/')
 
+%N = 1000;
+% trainIms = trainIms(:, :, :, 1:N);
+% trainLabels = trainLabels(:, :, :, 1:N);
+% testIms = testIms(:, :, :, 1:0.1*N);
+% testLabels = testLabels(:, :, :, 1:0.1*N);
 
 %% Custom network
 
 dataSize = size(trainIms);
 inputSize = dataSize(1:3);
+squareSize = 5;
 
 layers = [
-    imageInputLayer([inputSize], 'Normalization', 'none')
+    imageInputLayer([inputSize], 'Normalization', 'zerocenter')
     
-    convolution2dLayer([3, 3], 8 ,'Padding','same', 'Stride', 1)
+    convolution2dLayer([squareSize, squareSize], 2 * squareSize - 2 ,'Padding','same', 'Stride', 1)
+    batchNormalizationLayer
     reluLayer
     
     convolution2dLayer([3, 3], 16 ,'Padding','same', 'Stride', 1)
+    batchNormalizationLayer
+    maxPooling2dLayer([2, 2])
     reluLayer
     
-    convolution2dLayer([3, 3], 32, 'Padding','same', 'Stride', 1)
+    convolution2dLayer([3, 3], 32 ,'Padding','same', 'Stride', 1)
+    batchNormalizationLayer
+    maxPooling2dLayer([2, 2])
     reluLayer
     
-    convolution2dLayer([3, 3], 64, 'Padding','same', 'Stride', 1)
-    reluLayer
-            
     fullyConnectedLayer(32)
     reluLayer
     
     fullyConnectedLayer(1)
     regressionLayer];
 
+%% Specify convolutional weights
+
+layers(2).Weights = zeros(squareSize, squareSize, 1, 2*squareSize - 2);
+layers(2).Weights(:, :, 1, 1) = 0.1 * diag(ones(1, squareSize)) / sqrt(squareSize);
+rot = 1 / (squareSize - 1) * 90;
+for i = 1:2*squareSize - 3
+  layers(2).Weights(:, :, 1, i + 1) = ...
+    imrotate(layers(2).Weights(:, :, 1, 1), -i * rot, 'bilinear', 'crop');
+end
+
+layers(2).WeightLearnRateFactor = 0;
 
 %% Training parameters
 
-miniBatchSize = 16;
+miniBatchSize = 32;
 aachenDsTrain.MiniBatchSize = miniBatchSize;
 validationFreq = floor(length(trainLabels) / miniBatchSize);
 
-options = trainingOptions('adam', ...
+options = trainingOptions('sgdm', ...
     'MiniBatchSize',miniBatchSize, ...
     'MaxEpochs',30, ...
-    'InitialLearnRate',1e-3, ...
+    'InitialLearnRate',1e-4, ...
     'LearnRateSchedule','piecewise', ...
     'LearnRateDropFactor',0.5, ...
     'LearnRateDropPeriod',10, ...
@@ -57,3 +76,29 @@ options = trainingOptions('adam', ...
   
  %% Train network
 [net, trainInfo] = trainNetwork(trainIms, trainLabels, layers, options);
+
+%% Prediction
+vPredTrain = predict(net, trainIms, 'MiniBatchSize', miniBatchSize, 'ExecutionEnvironment', 'cpu');
+vPred = predict(net, testIms, 'MiniBatchSize', miniBatchSize, 'ExecutionEnvironment', 'cpu');
+
+%% Error in angle
+
+figure
+subplot(211)
+plot(squeeze(trainLabels), vPredTrain, '.')
+subplot(212)
+plot(squeeze(testLabels), vPred, '.')
+
+
+%%
+layer = 2;
+name = net.Layers(layer).Name;
+
+channels = 1:2*squareSize - 2;
+I = deepDreamImage(net,layer,channels,'PyramidLevels',1);
+
+figure
+for i = 1:8
+    subplot(3,3,i)
+    imshow(I(:,:,:,i))
+end
