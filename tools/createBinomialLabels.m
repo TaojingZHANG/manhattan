e61c5f1c146443ec1ec5596692bca6a7ee899df9
axel <axel.berg@arm.com>
2019-12-02 11:51:49 +0100

    Function to create binomial labels
    Signed-off-by: axel <axel.berg@arm.com>

---
+++ tools/createBinomialLabels.m
@@ -0,0 +1,16 @@
+function binLabels = createBinomialLabels(labels, classes)
+
+Nclasses = length(classes);
+L = floor(Nclasses / 4);
+
+binLabels = zeros(Nclasses, length(labels));
+for i = 1:length(labels)
+  d = abs(labels(i) - classes);
+  [~, argmin] = min(d);
+  binLabels(mod(argmin + (-L:L), Nclasses) + 1, i) = 1;
+end
+
+binLabels = reshape(binLabels, [1, 1, Nclasses, length(labels)]);
+
+end
+
